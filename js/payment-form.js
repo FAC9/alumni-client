e5aa510646bfe65d39c2ec534db4d291a252f05f
jsms90 <jsms@hotmail.co.uk>
2016-12-21 10:19:10 +0000

    add warning for incorrect email

---
+++ js/payment-form.js
@@ -0,0 +1,17 @@
+var donate = document.getElementsByClassName('button-container')[0];
+
+function verifyEmail() {
+  var emailAddress = document.getElementById('email').value;
+  console.log(emailAddress);
+  if (!/^[^W][a-zA-Z0-9_]+(.[a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+(.[a-zA-Z0-9_]+)*.[a-zA-Z]{2,4}$/.test(emailAddress)) {
+    document.getElementsByClassName('warning')[0].style.display = "block";
+    return false;
+  }
+  return true;
+}
+
+donate.onclick = function () {
+  if (verifyEmail()) {
+    window.open('./payment-confirmation.html');
+  }
+}
